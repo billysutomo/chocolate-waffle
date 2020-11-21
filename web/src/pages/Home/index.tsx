20f163408994e06e445b3de7c061aac2236ecf84
Billy Sutomo <billysutomo.53@gmail.com>
2020-11-22 01:08:09 +0700

    move camera icon to assets

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import { Element, ElementType, MessengerType } from "../../components/Element";
 import BasicLayout from "../../components/BasicLayout";
 
+import { ReactComponent as CameraIcon } from '../../assets/camera.svg';
+
 const ContainerStyled = styled.div`
   max-width: 400px;
   margin: 0 auto;
@@ -46,16 +48,6 @@ const TitleStyled = styled.div`
     color: white;
   }
 `;
-/*TODO: icon list
-
-- whatsapp - done
-- facebook
-- telegram
-- skype
-- viber
-- email
-- phone
-*/
 
 const dataDummy = [
   {
@@ -113,21 +105,7 @@ const Component: React.FC = () => {
       <ContainerStyled>
         <SvgContainerStyled>
           <SvgStyled>
-            <svg
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fal"
-              data-icon="camera"
-              className="svg-inline--fa fa-w-16 fa-camera"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-            >
-              <path
-                fill="currentColor"
-                d="M324.3 64c3.3 0 6.3 2.1 7.5 5.2l22.1 58.8H464c8.8 0 16 7.2 16 16v288c0 8.8-7.2 16-16 16H48c-8.8 0-16-7.2-16-16V144c0-8.8 7.2-16 16-16h110.2l20.1-53.6c2.3-6.2 8.3-10.4 15-10.4h131m0-32h-131c-20 0-37.9 12.4-44.9 31.1L136 96H48c-26.5 0-48 21.5-48 48v288c0 26.5 21.5 48 48 48h416c26.5 0 48-21.5 48-48V144c0-26.5-21.5-48-48-48h-88l-14.3-38c-5.8-15.7-20.7-26-37.4-26zM256 408c-66.2 0-120-53.8-120-120s53.8-120 120-120 120 53.8 120 120-53.8 120-120 120zm0-208c-48.5 0-88 39.5-88 88s39.5 88 88 88 88-39.5 88-88-39.5-88-88-88z"
-              />
-            </svg>
+            <CameraIcon />
           </SvgStyled>
         </SvgContainerStyled>
         <TitleStyled>
